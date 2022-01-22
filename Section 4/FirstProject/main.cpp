ce92b6771185f387052b629fdf64e41841fbe75d
harshedabdulla <harshed2004@gmail.com>
2022-01-22 13:10:40 +0530

    Second commit

---
+++ Section 4/FirstProject/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main(){
+    int favourite_number;
+    std::cout<<"Enter your favourite number: ";
+    std::cin>>favourite_number;
+    std::cout<<"Amazing its my favourite number too!"<<std::endl;
+    return 0;
+}
